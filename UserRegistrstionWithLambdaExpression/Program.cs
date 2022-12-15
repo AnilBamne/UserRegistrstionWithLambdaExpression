226a2c7cf44ab7a95862e8d8ca3fd85b449de484
AnilBamne <anilbamne20@gmail.com>
2022-12-15 19:26:08 +0530

    Adding:UC1FirstName code

--- UserRegistrstionWithLambdaExpression/Program.cs
+++ UserRegistrstionWithLambdaExpression/Program.cs
@@ -7,6 +7,25 @@ namespace UserRegistrstionWithLambdaExpression
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registrstion With Lambda Expression programs");
+            UserDetails user = new UserDetails();
+            Console.WriteLine("Choose option\n1:First Name\n2:Last Name\n3:Email");
+            int option=int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter first name");
+                    string firstName=Console.ReadLine();
+                    bool result = user.FirstNameValidate(firstName);
+                    Console.WriteLine(result);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+            
         }
     }
 }
---
+++ UserRegistrstionWithLambdaExpression/UserDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrstionWithLambdaExpression
+{
+    public class UserDetails
+    {
+        //Regex pattern for First name and Last name validation
+        public string regexForName = "^[A-Z][A-za-z]{2,}";
+
+        /// <summary>
+        /// Validating first name.
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <returns> true/false</returns>
+        public bool FirstNameValidate(string firstName) => Regex.IsMatch(firstName, regexForName);
+    }
+}
