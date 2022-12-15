70ec9b2c708d4a1dcc057d8ffd4316da6046ae47
AnilBamne <anilbamne20@gmail.com>
2022-12-15 18:02:10 +0530

    Adding Welcome message

---
+++ UserRegistrstionWithLambdaExpression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrstionWithLambdaExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registrstion With Lambda Expression programs");
+        }
+    }
+}
